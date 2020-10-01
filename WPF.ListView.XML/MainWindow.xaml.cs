ea5b51fd9c42d67eeec15f72f32386811367156e
Jesús Iván Hernández Ramírez <Jesus_Hdez.Ramirez@outlook.com>
2020-10-01 12:57:36 -0500

    Commit

---
+++ WPF.ListView.XML/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace WPF.ListView.XML
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
